8dc897a7b1a7dfbf340f7c365662e0cb4c8c320d
saarakoskinen <saaraidk@metropolia.com>
2025-01-15 18:56:24 +0200

    gittiin

---
+++ .venv/moduuli2/moduuli2_4.py
@@ -0,0 +1,13 @@
+luku1_str = input("Luku1: ")
+luku2_str = input("Luku2: ")
+luku3_str = input("Luku3: ")
+
+luku1 = float(luku1_str)
+luku2 = float(luku2_str)
+luku3 = float(luku3_str)
+
+summa = luku1 + luku2 + luku3
+tulo = luku1 * luku2 * luku3
+keskiarvo = (luku1 + luku2 + luku3) / 3
+
+print('Lukujen summa on:',str(summa) + ', tulo:', str(tulo) + ' ja keskiarvo:', str(keskiarvo))
