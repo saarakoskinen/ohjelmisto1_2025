2e9f03ae42712e701d483b65668ce8cadb1637b0
saarakoskinen <saaraidk@metropolia.com>
2025-01-24 14:28:45 +0200

    moduuli 4 tehtävä 5

--- .venv/moduuli4/moduuli4_5.py
+++ .venv/moduuli4/moduuli4_5.py
@@ -1,9 +1,12 @@
 kayttaja = input('Syötä käyttäjätunnus: ')
 salasana = input('Syötä salasana: ')
 
-while kayttaja != 'python' and salasana != 'rules':
+kokeilut = 0
+
+while kayttaja != 'python' and salasana != 'rules' and kokeilut < 4:
     print('Pääsy evätty.')
     kayttaja = input('Syötä käyttäjätunnus: ')
     salasana = input('Syötä salasana: ')
+    kokeilut = kokeilut + 1
     if kayttaja == 'python' and salasana == 'rules':
         print('Tervetuloa!')
